055b26865176ddbc515abda799ef6b3f315beb76
Konstantin Danilov <kdanilov@gmail.com>
2015-03-09 00:46:05 -0400

    Initial release

---
+++ Reverse/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Reverse
+{
+    public class Program
+    {
+        delegate string[] ReverseLines(List<string> lines);
+        
+        public static void Main(string[] args)
+        {
+            if (!args.Any())
+            {
+                Console.WriteLine("usage: reverse full-path-to-file [-r1 | -r2 | -r3]");
+                return;
+            }
+            
+            string filePath = args.First();
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File does not exist: {0}", filePath);
+                return;
+            }
+
+            var lines = new List<string>(File.ReadAllLines(filePath));
+            Console.WriteLine(String.Format("Total lines: {0}", lines.Count()));
+            
+            // magic happens here
+            ReverseLines reverseAlgorithm = GetReverseAlgorithm(args.Last());
+            File.WriteAllLines(filePath, reverseAlgorithm(lines));
+
+            Console.WriteLine("Reversed.");
+        }
+
+        private static ReverseLines GetReverseAlgorithm(string algorithmName)
+        {
+            switch (algorithmName)
+            {
+                case "-r2":
+                    return ReverseWithListMethods;
+                case "-r3":
+                    return ReverseManual;
+                default:
+                    return ReverseWithLINQ;
+            }
+        }
+
+        public static string[] ReverseWithLINQ(List<string> lines)
+        {
+            return lines.Reverse<string>().ToArray();
+        }
+
+        public static string[] ReverseWithListMethods(List<string> lines)
+        {
+            lines.Reverse();
+            return lines.ToArray();
+        }
+
+        public static string[] ReverseManual(List<string> lines)
+        {
+            string[] reversed = new string[lines.Count];
+            for(int i=lines.Count - 1; i>=0; i--)
+            {
+                reversed[i] = lines[lines.Count - 1 - i];
+            }
+            return reversed;
+        }
+    }
+}
---
+++ TestReverse/MainTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reverse;
+using System.Collections.Generic;
+
+namespace TestReverse
+{
+    [TestClass]
+    public class MainTest
+    {
+        [TestMethod]
+        public void TestReverseManualOdd()
+        {
+            string[] actual = new string[] { "1", "2", "3", "4", "5" };
+            string[] expected = new string[] { "5", "4", "3", "2", "1" };
+
+            var result = Program.ReverseManual(new List<string>(actual));
+            
+            for(int i=0; i < expected.Length; i++)
+                Assert.AreEqual(expected[i], result[i]);
+        }
+
+        [TestMethod]
+        public void TestReverseManualEven()
+        {
+            string[] actual = new string[] { "1", "2", "3", "4" };
+            string[] expected = new string[] { "4", "3", "2", "1" };
+
+            var result = Program.ReverseManual(new List<string>(actual));
+
+            for (int i = 0; i < expected.Length; i++)
+                Assert.AreEqual(expected[i], result[i]);
+        }
+
+        [TestMethod]
+        public void TestReverseManualEmpty()
+        {
+            string[] actual = new string[] {  };
+            string[] expected = new string[] {  };
+
+            var result = Program.ReverseManual(new List<string>(actual));
+            Assert.IsTrue(result.Length == 0);
+        }
+    }
+}
